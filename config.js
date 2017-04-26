a2cb406f21f3d1dcd09e38cf669bdfb7add88048
Mik Freedman <pair+mik@pivotal.io>
2017-04-25 21:41:33 -0600

    use cf services, and aws storage. remove development

--- config.js
+++ config.js
@@ -1,79 +1,54 @@
-var cfev = require("cfenv");
+var cfenv = require("cfenv");
 var appEnv = cfenv.getAppEnv();
-
-// # Ghost Configuration
-
 var path = require('path'),
-    config;
+  config;
+var pkg   = require("./package.json");
+var sqlCredentials = appEnv.getService("ghost-mysql").credentials;
+
+sqlCredentials.database = sqlCredentials.name
+sqlCredentials.user = sqlCredentials.username
+sqlCredentials.host = sqlCredentials.hostname
+
+var mailCredentials = appEnv.getService("ghost-mail").credentials;
+mailCredentials.transport = 'SMTP';
+
+mailCredentials.options = {
+  host: mailCredentials.hostname,
+  port: 25,
+  service: 'sendgrid',
+  auth: {
+    user: mailCredentials.username,
+    pass: mailCredentials.password
+  }
+}
 
 config = {
-    // ### Production
-    production: {
-        url: appEnv.url,
-        mail: {},
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost.db')
-            },
-            debug: false
-        },
-
-        server: {
-            host: '0.0.0.0',
-            port: appEnv.port,
-        }
+  production: {
+    url: appEnv.url,
+    mail: mailCredentials,
+    database: {
+      client: 'mysql',
+      connection: sqlCredentials,
+      debug: false
     },
-
-    // ### Development **(default)**
-    development: {
-        url: 'http://localhost:2368',
-
-        // #### Database
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-dev.db')
-            },
-            debug: false
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2368'
-        },
-        paths: {
-            contentPath: path.join(__dirname, '/content/')
-        }
+    server: {
+      host: '0.0.0.0',
+      port: appEnv.port,
     },
-
-    // **Developers only need to edit below here**
-
-    // ### Testing
-    // Used when developing Ghost to run tests and check the health of Ghost
-    // Uses a different port number
-    testing: {
-        url: 'http://127.0.0.1:2369',
-        database: {
-            client: 'sqlite3',
-            connection: {
-                filename: path.join(__dirname, '/content/data/ghost-test.db')
-            },
-            pool: {
-                afterCreate: function (conn, done) {
-                    conn.run('PRAGMA synchronous=OFF;' +
-                    'PRAGMA journal_mode=MEMORY;' +
-                    'PRAGMA locking_mode=EXCLUSIVE;' +
-                    'BEGIN EXCLUSIVE; COMMIT;', done);
-                }
-            },
-            useNullAsDefault: true
-        },
-        server: {
-            host: '127.0.0.1',
-            port: '2369'
-        },
-        logging: false
+    paths: {
+        contentPath: path.join(__dirname, 'content'),
     },
+    storage: {
+      active: 'ghost-s3',
+      'ghost-s3': {
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET,
+        bucket: process.env.AWS_BUCKET,
+        region: process.env.AWS_REGION,
+        assetHost: process.env.AWS_URL
+      }
+    }
+  }
 };
 
 module.exports = config;
